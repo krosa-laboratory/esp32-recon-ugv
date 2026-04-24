ba8ff2bb42d2b72b982690e69951fe45ec15d381
piratike <kevinmachuca96@hotmail.com>
2026-04-24 22:09:57 +0100

    Added motor funcionalities and tested together with the HTTP interface.

--- ReconScout_UGV/src/main.cpp
+++ ReconScout_UGV/src/main.cpp
@@ -4,6 +4,17 @@
 #include <WebSocketsServer.h>
 #include "WebPage.h"
 
+// Motor pins
+#define LEFT_MOTOR_IN1 12
+#define LEFT_MOTOR_IN2 13
+#define RIGHT_MOTOR_IN1 14
+#define RIGHT_MOTOR_IN2 15
+
+// PWM channels from ESP32
+#define CHANNEL_L1 0
+#define CHANNEL_L2 1
+#define CHANNEL_R1 2
+#define CHANNEL_R2 3
 
 // Access Points credentials
 const char* ssid = "ReconScout_UGV";
@@ -16,6 +27,9 @@ WebSocketsServer WebSocket = WebSocketsServer(81);
 // Global variables to store motor speeds
 int target_speed_left = 0;
 int target_speed_right = 0;
+
+// Global variable for security break
+unsigned long last_command_time = 0;
 
 
 void webSocketEventHandler(uint8_t number, WStype_t type, uint8_t* payload, size_t length) {
@@ -36,6 +50,72 @@ void webSocketEventHandler(uint8_t number, WStype_t type, uint8_t* payload, size
                 target_speed_left, target_speed_right
             );
 
+            last_command_time = millis();
+
+        }
+
+    }
+
+}
+
+
+void setupMotors() {
+
+    // Configure PWM properties
+    ledcSetup(CHANNEL_L1, 1000, 8);
+    ledcSetup(CHANNEL_L2, 1000, 8);
+    ledcSetup(CHANNEL_R1, 1000, 8);
+    ledcSetup(CHANNEL_R2, 1000, 8);
+    // Link logic channels with GPIOs
+    ledcAttachPin(LEFT_MOTOR_IN1, CHANNEL_L1);
+    ledcAttachPin(LEFT_MOTOR_IN2, CHANNEL_L2);
+    ledcAttachPin(RIGHT_MOTOR_IN1, CHANNEL_R1);
+    ledcAttachPin(RIGHT_MOTOR_IN2, CHANNEL_R2);
+    // Start with them stopped
+    ledcWrite(CHANNEL_L1, 0);
+    ledcWrite(CHANNEL_L2, 0);
+    ledcWrite(CHANNEL_R1, 0);
+    ledcWrite(CHANNEL_R2, 0);
+
+}
+
+
+void updateMotors() {
+
+    // --- LEFT Motor control
+    if (target_speed_left == 0) { // If speed is 0, it must be stopped
+        ledcWrite(CHANNEL_L1, 0);
+        ledcWrite(CHANNEL_L2, 0);
+    } else { // In this case we calculate the PWM value with the speed received
+
+        int left_pwm = map(abs(target_speed_left), 0, 100, 0, 255);
+        Serial.printf("-- LEFT PWM adjusted to %d\n", left_pwm);
+
+        if (target_speed_left > 0) {
+            ledcWrite(CHANNEL_L1, left_pwm);
+            ledcWrite(CHANNEL_L2, 0);
+        } else if (target_speed_left < 0) {
+            ledcWrite(CHANNEL_L1, 0);
+            ledcWrite(CHANNEL_L2, left_pwm);
+        }
+
+    }
+
+    // --- RIGHT Motor control
+    if (target_speed_right == 0) { // If speed is 0, it must be stopped
+        ledcWrite(CHANNEL_R1, 0);
+        ledcWrite(CHANNEL_R2, 0);
+    } else { // In this case we calculate the PWM value with the speed received
+
+        int right_pwm = map(abs(target_speed_right), 0, 100, 0, 255);
+        Serial.printf("-- RIGHT PWM adjusted to %d\n", right_pwm);
+
+        if (target_speed_right > 0) {
+            ledcWrite(CHANNEL_R1, right_pwm);
+            ledcWrite(CHANNEL_R2, 0);
+        } else if (target_speed_right < 0) {
+            ledcWrite(CHANNEL_R1, 0);
+            ledcWrite(CHANNEL_R2, right_pwm);
         }
 
     }
@@ -47,6 +127,8 @@ void setup() {
 
     Serial.begin(115200);
     Serial.println("--- Starting UGV...");
+
+    setupMotors();
 
     WiFi.softAP(ssid, pswd);
 
@@ -69,6 +151,19 @@ void setup() {
 
 void loop() {
 
+    // Maintain the connection openned
     WebSocket.loop();
+
+    // Execute motor commands
+    updateMotors();
+
+    // Watchdog if we lose connection
+    if (millis() - last_command_time > 200) {
+        if (target_speed_left != 0 || target_speed_right != 0) {
+            target_speed_left = 0;
+            target_speed_right = 0;
+            Serial.println("-- CUATION: Connection Lost! UGV Stopped.");
+        }
+    }
 
 }
